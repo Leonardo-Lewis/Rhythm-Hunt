618c6dd5580f0640749bcea6a2325c5d886fc353
Leonardo <leonardo.lee.lewis@gmail.com>
2021-06-02 09:51:16 +1000

    First mechanics of the game implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject weaponObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            Swing();
+        }
+    }
+
+    private void Swing()
+    {
+        Instantiate(weaponObject, transform.position, transform.rotation); 
     }
 }
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public float lifeTime;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
