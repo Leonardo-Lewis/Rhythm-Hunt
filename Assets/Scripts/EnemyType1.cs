83ed317bde32fa83bc9c347b5afc8e39175a9580
Leonardo <leonardo.lee.lewis@gmail.com>
2021-06-02 09:59:02 +1000

    Enemy can now be killed

---
+++ Assets/Scripts/EnemyType1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyType1 : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerWeapon")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
