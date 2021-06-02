d0ab3cb63ef816c51cd020f262e588c396810fc5
Leonardo <leonardo.lee.lewis@gmail.com>
2021-06-02 10:45:59 +1000

    Inclusion of second weapon and enemy, spawner exists but needs work

--- Assets/Scripts/PlayerControllerWeapon2.cs
+++ Assets/Scripts/PlayerControllerWeapon2.cs
@@ -2,27 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerWeapon2 : MonoBehaviour
 {
     public GameObject weaponObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
-        { 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
             Swing();
         }
     }
 
     private void Swing()
     {
-        Instantiate(weaponObject, transform.position, transform.rotation); 
+        Instantiate(weaponObject, transform.position, transform.rotation);
     }
 }
