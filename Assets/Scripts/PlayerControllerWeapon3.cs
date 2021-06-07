8fe116fefdd46868419f1c32f2ef07a8757d6b77
Leonardo <leonardo.lee.lewis@gmail.com>
2021-06-07 10:17:45 +1000

    Added 3rd lane

---
+++ Assets/Scripts/PlayerControllerWeapon3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerWeapon3 : MonoBehaviour
+{
+    public GameObject weaponObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Swing();
+        }
+    }
+
+    private void Swing()
+    {
+        Instantiate(weaponObject, transform.position, transform.rotation);
+    }
+}
