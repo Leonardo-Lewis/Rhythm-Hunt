bb30b5802debaa1072491bc786ad0823f16d33dd
Leonardo <leonardo.lee.lewis@gmail.com>
2021-06-23 12:09:08 +1000

    Renamed script

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public GameObject bloodSplatter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+
+    //Destroys the Game Object and spawns a particle system when colliding with objects marked "PLayer" or "Weapon"
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerWeapon")
+        {
+            Destroy(gameObject);
+            Instantiate(bloodSplatter, transform.position, transform.rotation);
+        }
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
